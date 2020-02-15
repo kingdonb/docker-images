462b825cbfe700f555eab72d2801ee4468e35a7c
Kingdon Barrett <kingdon.b@nd.edu>
2020-02-15 14:16:07 -0500

    you can also restart the oracle 18c XE database!

--- OracleDatabase/runme.sh
+++ OracleDatabase/runme.sh
@@ -2,6 +2,13 @@
 
 set -xe
 
+if docker ps -a --format '{{.Names}}'|egrep '^oracle-18c-xe$'; then
+
+  docker restart oracle-18c-xe &&
+    docker attach oracle-18c-xe
+
+else
+
 docker run --name oracle-18c-xe \
   -p 1521:1521 -p 5500:5500 \
   -e ORACLE_PWD=oracle \
@@ -9,3 +16,4 @@ docker run --name oracle-18c-xe \
   -v $(pwd)/oradata:/opt/oracle/oradata \
   netmoom:4999/oracle/database:18.4.0-xe
 
+fi
